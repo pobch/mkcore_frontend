4148780ffc03d3a140d78560e433cec52c7e5255
Pob <590650@gmail.com>
2018-05-09 13:53:24 +0700

    add temp logo at Home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
+import helloSvg from '../static/hello-2.svg'
+
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <h5>Home Page</h5>
-        <ul>
+        <img src={helloSvg} className="rounded img-fluid my-3" alt="Image" style={{width: '20rem'}}/>
+        <ul style={{maxWidth: '25rem'}}>
           <li>We have not implemented smtp server for production yet.</li>
           <li>Click Log in and use the test account on that page if you want to try our app.</li>
         </ul>
