5ca74cfddce7d0fd800d6d034a35f8e0ad86f425
Kulatuch XPS <kulatuch@studiotwist.co>
2018-06-10 13:55:08 +0700

    done not found

--- src/components/notfound.js
+++ src/components/notfound.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function NotFound(props) {
   return (
-    <div>
-      <h5>Not Found Page</h5>
+    <div className="wrapper">
+      <div className="loading fixed">
+        <div className="loading-wrapper align-center">
+          <i className="twf twf-exclamation-circle large-size" />
+          <div className="spacing-top">ไม่มีหน้าที่กำลังหา</div>
+        </div>
+      </div>
     </div>
   )
 }
